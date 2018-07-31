5543fcac71b119028470ff9ac1fda4ba59855c65
Jeru Kim <mikurej95@gmail.com>
2018-07-31 11:55:16 -0500

    modules created

--- bash.js
+++ bash.js
@@ -1,10 +1,7 @@
 process.stdout.write('prompt > ');
 
-process.stdin.on('data', (data) => {
-  const cmd = data.toString().trim();
-  if (cmd === 'pwd'){
-    process.stdout.write(__dirname);
-  };
-  process.stdout.write('\nprompt > ')
+// const fs = require('fs');
 
-});
+const pwd = require('./pwd.js');
+
+const ls = require('./ls.js');
